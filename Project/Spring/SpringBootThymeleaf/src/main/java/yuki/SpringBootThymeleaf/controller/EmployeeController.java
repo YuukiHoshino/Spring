0752fc193728bd7441326d0019ebd1f2a6912808
星野有軌 <yukihoshino1123@gmail.com>
2021-05-01 02:08:40 +0900

    delete, updateの機能追加

--- Project/Spring/SpringBootThymeleaf/src/main/java/yuki/SpringBootThymeleaf/controller/EmployeeController.java
+++ Project/Spring/SpringBootThymeleaf/src/main/java/yuki/SpringBootThymeleaf/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import yuki.SpringBootThymeleaf.bean.Employee;
 import yuki.SpringBootThymeleaf.service.EmployeeService;
@@ -52,6 +53,30 @@ public class EmployeeController {
 		// use a redirect to prevent duplicate submissions
 		return "redirect:/employeeController/showEmployees";
 	}
+
+	@GetMapping("/showUpdateForm")
+	public String showUpdateForm(@RequestParam("employeeId") int theId, Model theModel) {
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		// send over to our form
+		return "employees/employee-add-form";
+	}
+
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId) {
+
+		// delete the employee
+		employeeService.deleteById(theId);
+
+		// redirect to /employees/list
+		return "redirect:/employeeController/showEmployees";
+
+	}
+
 
 }
 
